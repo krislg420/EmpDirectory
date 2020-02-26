321c4c51b37f9328994a8be3a47d432a516484b4
krislg420 <57952065+krislg420@users.noreply.github.com>
2020-02-25 20:05:41 -0600

    added App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import SearchResultContainer from "./components/SearchResultContainer";
+
+function App() {
+  return <SearchResultContainer />;
+}
+
+export default App;
+
+
+
