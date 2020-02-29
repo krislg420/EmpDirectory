b84820818c5e7d8f24957ab38c07a02a6186610a
krislg420 <57952065+krislg420@users.noreply.github.com>
2020-02-29 02:15:51 -0600

    added app

---
+++ my-app/build/precache-manifest.b312fa76b199bd839fe231343f05721d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a6eb942191a66b546b67f9465c6aca",
+    "url": "/krislg420/EmpDirectory/index.html"
+  },
+  {
+    "revision": "b4d30ccb9857edfef929",
+    "url": "/krislg420/EmpDirectory/static/css/main.80e25bae.chunk.css"
+  },
+  {
+    "revision": "a47e80a5a39c50a3c542",
+    "url": "/krislg420/EmpDirectory/static/js/2.86ccfed4.chunk.js"
+  },
+  {
+    "revision": "d8080348ff2064724b36f2ca2f82999a",
+    "url": "/krislg420/EmpDirectory/static/js/2.86ccfed4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d30ccb9857edfef929",
+    "url": "/krislg420/EmpDirectory/static/js/main.c96c45d3.chunk.js"
+  },
+  {
+    "revision": "7ab0dcb90770f18cfe5c",
+    "url": "/krislg420/EmpDirectory/static/js/runtime-main.51925442.js"
+  }
+]);
